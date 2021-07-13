5c544409fe7478baa2eae623252ec02e5a384870
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-07-13 16:44:35 +0100

    add item fixed

--- Stores/settings.py
+++ Stores/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
